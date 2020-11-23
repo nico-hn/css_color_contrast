e918cb63cb760e26e78e5d8845d511c48a4157b0
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2020-11-23 23:25:14 +0900

    add CommandInterpreter::Parser#parse()

--- lib/css_color_contrast/command_interpreter.rb
+++ lib/css_color_contrast/command_interpreter.rb
@@ -6,21 +6,40 @@ require 'color_contrast_calc'
 module CssColorContrast
   module CommandInterpreter
     module TokenRe
+      FUNC_HEAD = /:\s*/
       LABEL = /[^\s:()]+/
+      SPACE = /\s+/
       COLOR_SCHEME = /(rgba?|hwb)/i
+    end
+
+    class Function
+      attr_reader :name
+      attr_reader :params
+
+      def initialize(name)
+        @name = name
+        @params = []
+      end
+
+      def push(param)
+        @params.push(param)
+      end
     end
 
     class Parser
       attr_reader :tokens
+      attr_reader :node_tree
 
       def initialize(line)
         @scanner = StringScanner.new(line)
         @tokens = []
+        @node_tree = []
       end
 
       def read_label
         cur_pos = @scanner.pos
         label = @scanner.scan(TokenRe::LABEL)
+        return unless label
         if TokenRe::COLOR_SCHEME.match?(label)
           @tokens.push read_color_function(cur_pos)
         else
@@ -31,6 +50,22 @@ module CssColorContrast
       def read_color_function(cur_pos)
         @scanner.pos = cur_pos
         color = ColorContrastCalc.color_from(@scanner.scan_until(/\)/))
+      end
+
+      def read_separator
+        if @scanner.scan(TokenRe::FUNC_HEAD)
+          @node_tree.push(Function.new(@tokens.pop))
+        elsif @scanner.scan(TokenRe::SPACE) || @scanner.eos?
+          @node_tree.last.push(@tokens.pop)
+        end
+
+        read_label
+        read_separator unless @tokens.empty?
+      end
+
+      def parse
+        read_label
+        read_separator
       end
     end
   end
--- spec/command_interpreter_spec.rb
+++ spec/command_interpreter_spec.rb
@@ -1,6 +1,7 @@
 require 'css_color_contrast/command_interpreter'
 
 Parser = CssColorContrast::CommandInterpreter::Parser
+Function = CssColorContrast::CommandInterpreter::Function
 
 RSpec.describe CssColorContrast do
   describe CssColorContrast::CommandInterpreter do
@@ -19,6 +20,16 @@ RSpec.describe CssColorContrast do
           colors.read_label
 
           expect(colors.tokens.first).to be_a(ColorContrastCalc::Color)
+        end
+      end
+
+      describe '#parse' do
+        it 'expects to parse a line of command' do
+          info_rgb.parse
+          func = info_rgb.node_tree.first
+
+          expect(func).to be_a(Function)
+          expect(func.params.first).to be_a(ColorContrastCalc::Color)
         end
       end
     end
